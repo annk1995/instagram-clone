d631816b4341c2507f3c14d0b3551b626c9880ec
annk1995 <kabuikariuki052@gmail.com>
2022-06-07 21:15:31 +0300

    admin

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Profile,Image,Comments,Follow
+
+
+admin.site.register(Profile)
+admin.site.register(Image)
+admin.site.register(Comments)
+admin.site.register(Follow)
